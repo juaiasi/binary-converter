e3dc41786870c6bb9bcd294721ec0038967b18ef
juaiasi <juaiasi@gmail.com>
2021-08-17 15:28:02 -0300

    Conversor funcionando para todos número

--- script.js
+++ script.js
@@ -13,37 +13,51 @@ function update(){
     /*Pega o número inserido*/
     let inputValue = document.getElementById("input").value;
 
+    /* BINARY TO DECIMAL*/
     /* Cria função para voltar a ser número e em seguida transforma inputValue em inputArray */
     let toNumber = num => Number(num);
     let inputArray = Array.from(String(inputValue), toNumber);
-
     /* transforma cada dígito binário em decimal e soma*/
-    
     let exponent = inputArray.length-1;
     let resultNumber = 0;
-
-    function binToDecConverter(digito){
+    function binaryDigit(digito){
         n = digito*Math.pow(2,exponent);
         resultNumber += n;
         exponent --
     }
-    function decToBinConverter(){
-        let binArray = [];
-        let dividedByTwo = Math.trunc(inputValue/2);
-        let reminder = inputValue%2;
-        console.log(dividedByTwo);
+    function binToDecConverter(){
+        inputArray.forEach(binaryDigit);
+    }
+
+    /* DECIMAL TO BINARY*/
+    let binArray = [];
+    let dividedByTwo = Math.trunc(inputValue/2);
+    let reminder = inputValue%2;
+    /* coloca o resto da divisão por 2 no começo da array*/
+    function fillBinArray(firstOfArray){
         while(dividedByTwo >= 1){
             binArray.unshift(reminder);
             reminder=dividedByTwo%2;
             dividedByTwo = Math.trunc(dividedByTwo/2);
         }
-        binArray.unshift(inputValue%2);
+        binArray.unshift(firstOfArray);
+    }
+
+    /* concatena */
+    function decToBinConverter(){
+        if(reminder !== 0 || inputValue == 0){
+            fillBinArray(reminder);
+        }else{
+            fillBinArray(1);
+        }
         resultNumber=parseInt(binArray.join(''));
     }
+
+    /*Verifica se está no modo decToBin ou binToDec e aplica a função*/
     if(decToBin == true){
         decToBinConverter();
     }else{
-        inputArray.forEach(binToDecConverter);
+        binToDecConverter();
     }
 
     result.innerHTML = `${resultNumber}`;
