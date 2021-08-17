ce4f6f0c4ff3bb58e0f5500740a6b65bc5fdb695
juaiasi <juaiasi@gmail.com>
2021-08-17 14:40:00 -0300

    Conversor de decimal para binário funcionando apenas para números que não estão na potência de 2

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 let inputElement = document.getElementById("input");
 let result = document.getElementById("result");
+let decToBin = false;
 
-inputElement.addEventListener('change', update);
-
-function update(){
-    let decimal =0;
-
-    /*Pega o número*/
-    let inputValue = document.getElementById("input").value;
-    console.log(inputValue);
-
-    /* Transforma em uma array */
-    let myFunc = num => Number(num);
-    let inputArray = Array.from(String(inputValue), myFunc);
-    console.log(inputArray);
-
-    /* transforma cada dígito binário em decimal e concatena*/
-    let position=inputArray.length-1;
-    function binToDec(number){
-        console.log(position);
-        digito = number*Math.pow(2,position);
-        decimal += digito;
-        position --
-    }
-
-    inputArray.forEach(binToDec);
-    result.innerHTML = `${decimal}`;
+function binToDecState(){
+    decToBin = false;
+}
+function decToBinState(){
+    decToBin = true;
 }
 
+function update(){
+    /*Pega o número inserido*/
+    let inputValue = document.getElementById("input").value;
 
+    /* Cria função para voltar a ser número e em seguida transforma inputValue em inputArray */
+    let toNumber = num => Number(num);
+    let inputArray = Array.from(String(inputValue), toNumber);
+
+    /* transforma cada dígito binário em decimal e soma*/
+    
+    let exponent = inputArray.length-1;
+    let resultNumber = 0;
+
+    function binToDecConverter(digito){
+        n = digito*Math.pow(2,exponent);
+        resultNumber += n;
+        exponent --
+    }
+    function decToBinConverter(){
+        let binArray = [];
+        let dividedByTwo = Math.trunc(inputValue/2);
+        let reminder = inputValue%2;
+        console.log(dividedByTwo);
+        while(dividedByTwo >= 1){
+            binArray.unshift(reminder);
+            reminder=dividedByTwo%2;
+            dividedByTwo = Math.trunc(dividedByTwo/2);
+        }
+        binArray.unshift(inputValue%2);
+        resultNumber=parseInt(binArray.join(''));
+    }
+    if(decToBin == true){
+        decToBinConverter();
+    }else{
+        inputArray.forEach(binToDecConverter);
+    }
+
+    result.innerHTML = `${resultNumber}`;
+}
+
+inputElement.addEventListener('change', update);
